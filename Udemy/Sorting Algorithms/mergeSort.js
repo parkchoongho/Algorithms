64ee3261979103222552040ef3ed1ff3c5024580
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-10-13 23:20:15 +0900

    update mergeSort.js

--- Udemy/Sorting Algorithms/mergeSort.js
+++ Udemy/Sorting Algorithms/mergeSort.js
@@ -1,4 +1,4 @@
-function mergeSort(arr1, arr2) {
+function merge(arr1, arr2) {
   let results = [];
   let arr1Index = 0;
   let arr2Index = 0;
@@ -25,4 +25,12 @@ function mergeSort(arr1, arr2) {
   return results;
 }
 
-console.log(mergeSort([1, 4, 6], [3, 4, 8, 9]));
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([1, 6, 3, 4, 8, 9, 7, 5, 2]));
