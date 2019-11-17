5a883e9bbaa3855e6ac1f4cb0506072a465b132b
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-11-17 20:38:46 +0900

    add insert method

--- Udemy/Trees/binarySearchTrees.js
+++ Udemy/Trees/binarySearchTrees.js
@@ -1,9 +1,3 @@
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-}
-
 class Node {
   constructor(val) {
     this.val = val;
@@ -12,8 +6,41 @@ class Node {
   }
 }
 
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (currentNode.val === val) return undefined;
+      if (currentNode.val < val) {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          break;
+        }
+        currentNode = currentNode.right;
+      } else {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          break;
+        }
+        currentNode = currentNode.left;
+      }
+    }
+  }
+}
+
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(12);
+tree.insert(9);
+// tree.insert(11);
+// tree.insert(11);
+
+console.dir(tree);
