c30ea01b06250b9f883e6200aab3a42faed41dd2
parkchoongho <pchyo92@gmail.com>
2021-02-17 00:09:11 +0900

    링크드리스트 pop 구현

--- Algoexprt/linked_list.py
+++ Algoexprt/linked_list.py
@@ -11,29 +11,33 @@ class LinkedList():
         new_node = Node(val)
         if self.head == None:
             self.head = new_node
-        else:
-            current_node = self.head
-            while current_node.next != None:
-                current_node = current_node.next
-            current_node.next = new_node
-
-    def delete(self, data):
+            return None
         current_node = self.head
-        prev_node = self.head
-        next_node = self.head
         while current_node.next != None:
-            if current_node == self.head:
-                current_node = current_node.next
-                next_node = current_node.next
-                continue
-            prev_node = current_node
-            current_node = next_node
-            next_node = next_node.next
+            current_node = current_node.next
+        current_node.next = new_node
 
+    def pop(self):
+        last_node = self.head
+        # 링크드리스트에 아무런 노드가 없는 경우
+        if last_node == None:
+            return None
+        # 헤드에만 노드가 있는 경우
+        if last_node.next == None:
+            self.head = None
+            return last_node.val
+        prev_node = self.head
+        while last_node.next !=None:
+            prev_node = last_node
+            last_node = last_node.next
+        prev_node.next = None
+        return last_node.val
 
 
     def __str__(self):
         current_node = self.head
+        if current_node == None:
+            return "{}"
         output = "{"
         output += str(current_node.val)
         while current_node.next != None:
@@ -49,6 +53,12 @@ linked_list.append(2)
 linked_list.append(1)
 linked_list.append(27)
 linked_list.append(30)
+
+print(linked_list.pop())
+print(linked_list.pop())
+print(linked_list.pop())
+print(linked_list.pop())
+print(linked_list.pop())
 
 print(linked_list)
 
