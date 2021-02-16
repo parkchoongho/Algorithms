9784aef93516b7279a5de09e0b1eaca3f39879a0
parkchoongho <pchyo92@gmail.com>
2021-02-17 00:28:11 +0900

    링크드리스트 shift unshift 구현

--- Algoexprt/linked_list.py
+++ Algoexprt/linked_list.py
@@ -33,6 +33,19 @@ class LinkedList():
         prev_node.next = None
         return last_node.val
 
+    def shift(self):
+        first_node = self.head
+        # 링크드리스트에 아무런 노드가 없는 경우
+        if first_node == None:
+            return None
+        self.head = first_node.next
+        return first_node.val
+
+    def unshift(self, val):
+        new_node = Node(val)
+        new_node.next = self.head
+        self.head = new_node
+
 
     def __str__(self):
         current_node = self.head
@@ -54,11 +67,17 @@ linked_list.append(1)
 linked_list.append(27)
 linked_list.append(30)
 
-print(linked_list.pop())
-print(linked_list.pop())
-print(linked_list.pop())
-print(linked_list.pop())
-print(linked_list.pop())
+print(linked_list.shift())
+print(linked_list.shift())
+print(linked_list.shift())
+print(linked_list.shift())
+print(linked_list.shift())
+
+linked_list.unshift(3)
+linked_list.unshift(2)
+linked_list.unshift(1)
+linked_list.unshift(56)
+
 
 print(linked_list)
 
