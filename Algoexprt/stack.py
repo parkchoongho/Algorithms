824e2f528fea1044479b15797518d4ebd9af6b74
parkchoongho <pchyo92@gmail.com>
2021-03-03 20:34:40 +0900

    stack push pop search method 구현

--- Algoexprt/stack.py
+++ Algoexprt/stack.py
@@ -1,33 +1,66 @@
-class Node():
+from abc import abstractmethod
+
+
+class NoSuchElementException(Exception):
+    pass
+
+
+class Node:
     def __init__(self, val):
         self.val = val
-        self.next = None
+        self.prev = None
 
-class Stack():
+
+class StackInterface:
+    @abstractmethod
+    def push(self, node: Node) -> None:
+        pass
+
+    @abstractmethod
+    def pop(self) -> Node:
+        pass
+
+    @abstractmethod
+    def search(self, index) -> Node:
+        pass
+
+
+class Stack(StackInterface):
     def __init__(self):
         self.first = None
         self.last = None
         self.size = 0
 
-    def push(val):
-        new_node = Node(val)
-        if self.first ==None:
-            self.first = new_node
-            self.last = new_node
-        else:
-            temp = self.first
-            self.first = new_node
-            self.first.next = temp
+    def push(self, node: Node) -> None:
         self.size += 1
-        return self.size
+        if self.first is None:
+            self.first = node
+            self.last = node
+            return
+        node.prev = self.last
+        self.last = node
+        return
 
-    def pop():
-        if self.first == None:
-            return None
-        temp = self.first
-        if self.first == self.last:
-            self.last = None
-        self.first = self.first.next
+    def pop(self) -> Node:
+        if self.last is None:
+            raise NoSuchElementException()
         self.size -= 1
-        temp.next = None
-        return temp.val
+        if self.first is self.last:
+            node = self.last
+            self.first = None
+            self.last = None
+            return node
+        node = self.last
+        self.last = self.last.prev
+        node.prev = None
+        return node
+
+    def search(self, index) -> Node:
+        if index > self.size:
+            raise NoSuchElementException()
+        start_index = 1
+        node = self.last
+        while start_index != index:
+            start_index += 1
+            node = node.prev
+        return node
